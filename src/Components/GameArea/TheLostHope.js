3c6df5c10263cb4aa6fbe2a007e0d7594c719222
Berkay Yıldız <berkay4421@gmail.com>
2023-05-31 04:05:51 +0300

    fixed the lost hope text, index html and manifest.json

--- src/Components/GameArea/TheLostHope.js
+++ src/Components/GameArea/TheLostHope.js
@@ -12,7 +12,7 @@ function TheLostHope() {
           <div className="thelosthope-part-one">
             <h1>CO-OP FANTASTIC FEAR GAME</h1>
             <p>
-              The Lost Hope" is a fantastical horror co-op game where players
+              The Lost Hope is a fantastical horror co-op game where players
               are tasked with completing designated missions and reaching the
               ultimate goal. The game features four characters, and in order to
               progress, players must work together and overcome various
